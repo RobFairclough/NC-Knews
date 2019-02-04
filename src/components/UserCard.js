a5686df3583fd0564779f4d95579d3164b9bf583
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-02-04 16:50:12 +0000

    fix border-radius for cards

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -4,10 +4,17 @@ const UserCard = props => {
   const { username, avatar_url, name } = props.user;
   return (
     <li className="user-card">
-      <img className="user-avatar" alt={`${name}'s avatar`} src={avatar_url} />
+      <a href={`/users/${username}`}>
+        <img
+          className="user-avatar"
+          alt={`${name}'s avatar`}
+          src={avatar_url}
+        />
+      </a>
       <span>
         Username: {username} <br /> Name: {name}
       </span>
+      <br />
     </li>
   );
 };
