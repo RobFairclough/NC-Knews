4a77672cbdff8f1f3ea9844b8390250064c027e2
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-02-07 17:07:18 +0000

    add author img to article cards

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -3,15 +3,21 @@ import { Link } from '@reach/router';
 
 const ArticleCard = ({ article }) => (
   <li className="article-card">
-    <p className="article-topic-text">{article.topic}</p>
+    <i className="article-card-text">{article.title}</i>
+    <img
+      className="article-card-img"
+      alt="author portrait"
+      src={article.avatar_url}
+    />
     <i className="article-card-text">
-      {article.title} <br /> by
+      by
       <Link to={`/users/${article.author}`}> {article.author}</Link> <br />
       Written on {article.created_at}
     </i>
     <Link to={`/articles/${article.article_id}`} className="article-button">
       Check it out
     </Link>
+    <p className="article-topic-text">{article.topic}</p>
   </li>
 );
 
