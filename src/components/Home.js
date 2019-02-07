16c96538befa997ab1b202a889fac65f5d7fc3a9
Rob Fairclough <rob.a.fairclough@gmail.com>
2019-02-07 13:44:00 +0000

    homepage switched to hook

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,51 +1,48 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { fetchData } from '../api';
 import ArticleCard from './ArticleCard';
 import '../css/Articles.css';
-class Home extends Component {
-  state = {
-    latestArticles: '',
-    topArticles: ''
-  };
-  async componentDidMount() {
-    const { articles: latestArticles } = await fetchData(
-      `api/articles?limit=3`
-    );
-    const { articles: topArticles } = await fetchData(
-      `api/articles?sort_by=votes&limit=3`
-    );
-    this.setState({ latestArticles, topArticles });
-  }
-  async componentDidUpdate(prevProps, prevState) {}
-  render() {
-    const { latestArticles, topArticles } = this.state;
 
-    return (
-      <div>
-        <h1>NC News</h1>
-        <h2>The latest.</h2>
-        {latestArticles ? (
-          <ul className="articles-list">
-            {latestArticles.map(article => (
-              <ArticleCard key={article.article_id} article={article} />
-            ))}
-          </ul>
-        ) : (
-          <p>loading...</p>
-        )}
-        <h2>The Best.</h2>
-        {topArticles ? (
-          <ul className="articles-list">
-            {topArticles.map(article => (
-              <ArticleCard key={article.article_id} article={article} />
-            ))}
-          </ul>
-        ) : (
-          <p>loading...</p>
-        )}
-      </div>
-    );
-  }
-}
+const Home = () => {
+  const [latest, setLatest] = useState(null);
+  const [top, setTop] = useState(null);
+  const fetchNews = async () => {
+    const latestNews = await fetchData('api/articles', ['limit=3']);
+    const topNews = await fetchData('api/articles', [
+      'limit=3',
+      'sort_by=votes'
+    ]);
+    setLatest(latestNews.articles);
+    setTop(topNews.articles);
+  };
+  useEffect(() => {
+    fetchNews();
+  }, []);
+  return (
+    <div>
+      <h1>NC News</h1>
+      <h2>The latest.</h2>
+      {latest ? (
+        <ul className="articles-list">
+          {latest.map(article => (
+            <ArticleCard key={article.article_id} article={article} />
+          ))}
+        </ul>
+      ) : (
+        <p>loading...</p>
+      )}
+      <h2>The Best.</h2>
+      {top ? (
+        <ul className="articles-list">
+          {top.map(article => (
+            <ArticleCard key={article.article_id} article={article} />
+          ))}
+        </ul>
+      ) : (
+        <p>loading...</p>
+      )}
+    </div>
+  );
+};
 
 export default Home;
